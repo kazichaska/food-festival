90b9b4f2516422e9b1aecec1db6f7243cec3fd67
Kazi Islam <kazichaska@gmail.com>
2022-04-05 21:40:34 -0500

    PWA enabled and works

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
     entry: {
@@ -52,6 +52,21 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report `named report.html` outputs to an HTML file in the dist folder
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events",
+            background_color: "#01579b",
+            start_url: "../index.html",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve("assets/img/icons/icon-512x512.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
         })
     ],
     mode: 'development',
